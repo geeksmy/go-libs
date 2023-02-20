ebf345e1be2c244295861e18dd78f5df1f02d964
Kevin Ma <kevin_ma@ycsoftwares.com>
2023-02-20 21:53:36 +0800

    [feat] 新增json分页

--- gorm/pagination.go
+++ gorm/pagination.go
@@ -70,3 +70,50 @@ func Pagination(db *gorm.DB, limit, offset int, result interface{}) (*Page, erro
 
 	return &page, nil
 }
+
+func PaginationJoin(db *gorm.DB, limit, offset int, model, result interface{}) (*Page, error) {
+	// 如果每页条数<=0,则初始化为10条
+	if limit <= 0 {
+		limit = 10
+	}
+	// 如果偏移量小于0，则从0开始
+	if offset < 0 {
+		offset = 0
+	}
+
+	if result == nil {
+		return nil, ErrPageParamError
+	}
+
+	var (
+		page  = Page{}
+		count int64
+	)
+
+	err := db.Model(model).Count(&count).Error
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Limit(limit).Offset(offset).Find(result).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	page.TotalRecord = count
+	page.Records = result
+
+	page.Offset = offset
+	page.Limit = limit
+	page.TotalPage = int(math.Ceil(float64(count) / float64(limit)))
+
+	if count > int64(limit+offset) {
+		nextCursor := offset + limit
+		page.NextCursor = nextCursor
+	} else {
+		page.NextCursor = 0
+	}
+
+	return &page, nil
+}
